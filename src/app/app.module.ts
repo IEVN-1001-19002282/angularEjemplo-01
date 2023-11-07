cfb6830ff487caf28d2c9760375fb36ab6fdb190
IEVN-1001-19002282 <19002282@alumnos.utleon.edu.mx>
2023-11-06 18:57:56 -0600

    06/11/2023 - AGD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MenuComponent } from './menu/menu/menu.component';
 import { MaterialModule } from './material/material.module';
 import { AgregarComponent } from './utl/agregar/agregar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
